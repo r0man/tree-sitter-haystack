4252f98080eda589dc1c5b60db5daf3829217540
Roman Scherer <roman@burningswell.com>
2025-02-07 17:04:19 +0100

    Update commit

--- .guix/tree-sitter-hackstack-package.scm
+++ .guix/tree-sitter-hackstack-package.scm
@@ -8,11 +8,11 @@
   (@@ (gnu packages tree-sitter) tree-sitter-grammar))
 
 (define-public tree-sitter-haystack
-  (let ((commit "419fa3698350a052a060d4fcb0a72fe0807b5a06")
+  (let ((commit "8d7cb47f3e44f2e23574a7a7fcb2a5bb21a4c0ce")
         (revision "0"))
     (tree-sitter-grammar
      "haystack" "Haystack"
-     "1w80b7xii494rf9b9msy7jbagxsqq25xxz5srhslw9h82b9qzywl"
+     "06hlwwf1w69qv04njn6x3s2h7gvf8hca2d6iiy9s8xj4am99lij2"
      (git-version "0.0.1" revision commit)
      #:repository-url "https://github.com/r0man/tree-sitter-haystack"
      #:commit commit)))
